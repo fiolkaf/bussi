bada749229070c5b2c05c0092efc5ccf2eda23f9
Filip <fiolkaf@gmail.com>
2015-01-23 00:30:37 +0100

    RequireJS for Karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -10,19 +10,21 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'requirejs'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      {pattern: 'tests/*.js', included: true}
+      {pattern: 'tests/*.js', included: false},
+      'test-config/bind-polyfill.js', // For PhantomJS and unexpected support
+      {pattern: 'node_modules/unexpected/unexpected.js', included: false},
+      'test-config/test-main.js'
     ],
 
 
     // list of files to exclude
     exclude: [
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -46,7 +48,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
 
 
     // enable / disable watching file and executing tests whenever any file changes
@@ -55,11 +57,11 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,7 @@
-console.log('loaded');
-var unexpected = require('unexpected');
-var expect = weknowhow.expect;
-describe('karma tests with expect', function() {
-    it('should expose expect method', function() {
-        expect('foo', 'not to equal', 'bar');
+define(['node_modules/unexpected/unexpected'], function(expect) {
+    describe('karma tests with expect', function() {
+        it('should expose expect method', function() {
+            expect('foo', 'not to equal', 'bar');
+        });
     });
 });
